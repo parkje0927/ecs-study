fe43899ba19dd4d0793a1a8c24fb31c7b1d6b75e
parkje0927 <parkje09270@gmail.com>
2023-06-06 21:34:44 +0900

    feat: health-check 엔드포인트 주소 변경

--- src/main/java/com/study/ecs/controller/StatusController.java
+++ src/main/java/com/study/ecs/controller/StatusController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping
+    @GetMapping("/health-check")
     public String healthCheck() {
         return "hello";
     }
